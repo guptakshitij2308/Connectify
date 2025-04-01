b8cea949021569100d56d522fee4d5a45ff43e0e
Kshitij <guptakshitij2308@gmail.com>
2025-04-01 15:24:13 +0530

    Added chat controller and message controller.

---
+++ Connectify-Backend/src/main/java/com/connectify/models/Room.java
@@ -0,0 +1,44 @@
+package com.connectify.models;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "rooms")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Room {
+    @Id
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    private String roomId;
+    private List<Message>messages= new ArrayList<>();
+}
