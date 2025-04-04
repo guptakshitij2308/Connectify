a88fe835c6c884fdeb86e6dd25d5bcda5eb05e09
Kshitij <guptakshitij2308@gmail.com>
2025-04-05 00:25:58 +0530

    Added docker file.

---
+++ Connectify-Backend/src/main/java/com/connectify/config/AppConstants.java
@@ -0,0 +1,6 @@
+package com.connectify.config;
+
+public class AppConstants {
+    public static final String FRONT_END_BASE_URL="http://localhost:3000";
+    public static final String DB_URL="";
+}
