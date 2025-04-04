85a5bc3638dd21618e98d19aa6e3f29f6ba03433
Kshitij <guptakshitij2308@gmail.com>
2025-04-05 01:07:38 +0530

    Changed base url.

--- connectify-frontend/app/cofig/AxiosHelper.js
+++ connectify-frontend/app/cofig/AxiosHelper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8080";
+export const baseURL = "https://connectify-pxky.onrender.com";
 
 export const httpClient = axios.create({
   baseURL: baseURL,
